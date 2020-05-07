f383216058ff8703fa8c2a1a0fd6c4c696a7ef9d
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-05-06 20:17:34 -0700

    Added top main div with jQuery, text reflects API pull

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
 
 
+
+
+
+
     $(".closebtn").click(function () {
         $("#sidebar").css("width", "0")
         $("#main").css("marginLeft", "0")
@@ -10,44 +14,51 @@ $(document).ready(function () {
         $("#main").css("marginLeft", "250px")
     });
 
-    function weatherAPI() {
-
-        var apiKey = "6ed66ab8536af2098115f5cf3916bab8"
-        var cityName = "Sacramento"
-        var state = "CA"
-        var country = "US"
-        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "," + state + "," + country + "&appid=" + apiKey
-
-
-        // var queryURL = "http://api.openweathermap.org/data/2.5/weather?id=5389489&appid=6ed66ab8536af2098115f5cf3916bab8"
-        // api.openweathermap.org/data/2.5/weather?q=rocklin,ca&appid=6ed66ab8536af2098115f5cf3916bab8
-
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-            console.log(response.main.temp);
-            console.log(response.main.humidity);
-            console.log(response.wind.speed);
-            console.log(response.name + " " + response.sys.country);
-            console.log(response.weather[0].icon);
-            console.log(response.id);
-            console.log(Math.floor(((((response.main.temp) - 273.15) * 1.8) + 32)) + "\xB0" + "F"
-            );
 
 
 
-        });
+    function results(weatherType, weatherCall, weatherID) {
+        $("#weather-info").append(($("<div>").text(weatherType).addClass("weather-results")).append($("<span>").text(weatherCall).attr("id", weatherID)))
     }
 
-    weatherAPI();
+
+
+    var apiKey = "6ed66ab8536af2098115f5cf3916bab8"
+    var cityName = "Sacramento"
+    var state = "CA"
+    var country = "US"
+    var queryMainURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "," + state + "," + country + "&appid=" + apiKey
+
+
+
+    $.ajax({
+        url: queryMainURL,
+        method: "GET"
+    }).then(function (response) {
+        var lat = response.coord.lat
+        var lon = response.coord.lon
+        console.log(response);
+        console.log(response.id);
+        results("Location: ", response.name + ", " + response.sys.country, "location");
+        results("Temperature: ", Math.floor(((((response.main.temp) - 273.15) * 1.8) + 32)) + "\xB0" + "F", "temp");
+        results("Humidity: ", response.main.humidity.toFixed(2) + "%", "humidity");
+        results("Wind Speed: ", (response.wind.speed * 2.237).toFixed(2) + " mph", "wind-speed");
+
+        var queryUV = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey
+
+        $.ajax({
+            url: queryUV,
+            method: "GET"
+        }).then(function (response) {
+            results("UV Index: ", response.value, "UV-Index");
+        });
 
 
 
 
 
+
+    });
 
 
 
